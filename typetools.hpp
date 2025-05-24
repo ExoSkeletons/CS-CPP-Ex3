d564d987c1c8729bc14c93e283ce19d6ea20f800
ExoSkeleton <calazulazu@gmail.com>
2025-05-24 23:54:39 +0300

    Add removeValue

--- typetools.hpp
+++ typetools.hpp
@@ -5,9 +5,20 @@
 #ifndef TYPETOOLS_HPP
 #define TYPETOOLS_HPP
 
+#include <algorithm>
+
 template<typename Base, typename T>
-bool instanceof(const T *ptr) {
-    return dynamic_cast<const Base*>(ptr) != nullptr;
+bool instanceof(const T *ptr) { return dynamic_cast<const Base *>(ptr) != nullptr; }
+
+template<typename T>
+bool removeValue(std::vector<T> &vec, const T value) {
+    typename std::vector<T>::iterator position = std::find(vec.begin(), vec.end(), value);
+
+    // == myVector.end() means the element was not found
+    if (position == vec.end()) return false;
+
+    vec.erase(position);
+    return true;
 }
 
 #endif //TYPETOOLS_HPP
